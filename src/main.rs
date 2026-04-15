1a10d283e0ef244577c14de72d0745f6c95020e9
Tauseef Khan <tauseef25@gmail.com>
2026-04-14 22:36:38 -0700

    parse obj

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod grid_position;
 mod line;
+mod obj;
 
-use std::{fs, fs::File, io::Write, path::Path};
+use std::{
+    fs::File,
+    io::{self, Write},
+    path::Path,
+};
 
 use anyhow::Error;
-use clap::{Arg, command};
-use rand::RngExt;
 use tgar::{BGRA, PixelBGRA};
-
-use crate::grid_position::GridPosition;
-use line::line;
 
 const BLACK: PixelBGRA = PixelBGRA {
     b: 0,
@@ -56,55 +56,17 @@ const YELLOW: PixelBGRA = PixelBGRA {
 // images render upside down
 // compared to the reference implementation
 fn main() -> Result<(), Error> {
-    let matches = command!()
-        .arg(
-            Arg::new("obj")
-                .help("path to a .obj file to load")
-                .required(true)
-                .index(1),
-        )
-        .get_matches();
-
-    let obj_path = matches
-        .get_one::<String>("obj")
-        .ok_or_else(|| Error::msg("missing obj path"))?;
-    let _obj_src = fs::read_to_string(obj_path)?;
+    let stdin = io::stdin();
+    let _mesh = obj::parse(stdin.lock())?;
 
     let width: u16 = 64;
     let height: u16 = 64;
 
-    let mut frame_buffer = vec![BLACK; (width * height) as usize];
-
-    let mut rng = rand::rng();
-
-    for _ in 0..16_000_000 {
-        let a = GridPosition {
-            x: rng.random::<u16>() % width,
-            y: rng.random::<u16>() % height,
-        };
-        let b = GridPosition {
-            x: rng.random::<u16>() % width,
-            y: rng.random::<u16>() % height,
-        };
-
-        line(
-            a,
-            b,
-            &mut frame_buffer,
-            width,
-            PixelBGRA {
-                b: rng.random::<u8>(),
-                g: rng.random::<u8>(),
-                r: rng.random::<u8>(),
-                a: 255,
-            },
-        );
-    }
-
+    let frame_buffer = vec![BLACK; (width * height) as usize];
     let frame_buffer: BGRA = BGRA::new(width, height, &frame_buffer);
 
     let mut file = File::create(Path::new("framebuffer.tga"))?;
-    file.write(&frame_buffer.into_data()).unwrap();
+    file.write_all(&frame_buffer.into_data())?;
 
     Ok(())
 }
---
+++ src/obj.rs
@@ -0,0 +1,70 @@
+use std::io::BufRead;
+
+use anyhow::Error;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vertex {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Face {
+    pub vertices: Vec<usize>,
+}
+
+#[derive(Debug, Default)]
+pub struct Mesh {
+    pub vertices: Vec<Vertex>,
+    pub faces: Vec<Face>,
+}
+
+pub fn parse(mut reader: impl BufRead) -> Result<Mesh, Error> {
+    let mut mesh = Mesh::default();
+    let mut buf = String::new();
+
+    loop {
+        buf.clear();
+        if reader.read_line(&mut buf)? == 0 {
+            break;
+        }
+
+        let mut tokens = buf.split_whitespace();
+        match tokens.next() {
+            Some("v") => {
+                let x: f32 = tokens
+                    .next()
+                    .ok_or_else(|| Error::msg("v: missing x"))?
+                    .parse()?;
+                let y: f32 = tokens
+                    .next()
+                    .ok_or_else(|| Error::msg("v: missing y"))?
+                    .parse()?;
+                let z: f32 = tokens
+                    .next()
+                    .ok_or_else(|| Error::msg("v: missing z"))?
+                    .parse()?;
+                mesh.vertices.push(Vertex { x, y, z });
+            }
+            Some("f") => {
+                let mut indices = Vec::new();
+                for tok in tokens {
+                    let vert = tok
+                        .split('/')
+                        .next()
+                        .ok_or_else(|| Error::msg("f: empty token"))?;
+                    let idx: usize = vert.parse()?;
+                    if idx == 0 {
+                        return Err(Error::msg("f: zero index"));
+                    }
+                    indices.push(idx - 1);
+                }
+                mesh.faces.push(Face { vertices: indices });
+            }
+            _ => {}
+        }
+    }
+
+    Ok(mesh)
+}
