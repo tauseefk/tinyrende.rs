684d5c1910e304fdbb1ecba3fbbba235f6b3949c
Tauseef Khan <tauseef25@gmail.com>
2026-04-15 22:51:11 -0700

    multiple variants

--- src/line.rs
+++ src/line.rs
@@ -44,3 +44,16 @@ pub fn line(
         }
     }
 }
+
+pub fn triangle(
+    a: GridPosition,
+    b: GridPosition,
+    c: GridPosition,
+    pixel_data: &mut [PixelBGRA],
+    width: u16,
+    color: PixelBGRA,
+) {
+    line(a, b, pixel_data, width, color);
+    line(b, c, pixel_data, width, color);
+    line(c, a, pixel_data, width, color);
+}
