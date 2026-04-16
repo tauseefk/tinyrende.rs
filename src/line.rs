1887675dadd90b1a20cc6fb6d0496fa86900e78e
Tauseef Khan <tauseef25@gmail.com>
2026-04-15 22:50:31 -0700

    cleanup

--- src/line.rs
+++ src/line.rs
@@ -10,9 +10,9 @@ pub fn line(
     color: PixelBGRA,
 ) {
     // steep slope
-    let steep = a.x.abs_diff(b.x) < a.y.abs_diff(b.y);
+    let is_steep_slope = a.x.abs_diff(b.x) < a.y.abs_diff(b.y);
 
-    let (a, b) = if steep {
+    let (a, b) = if is_steep_slope {
         (a.transpose(), b.transpose())
     } else {
         (a, b)
@@ -30,7 +30,7 @@ pub fn line(
 
     for x in a.x..=b.x {
         let y_u16 = y as u16;
-        let coord = if steep {
+        let coord = if is_steep_slope {
             GridPosition { x: y_u16, y: x }
         } else {
             GridPosition { x, y: y_u16 }
